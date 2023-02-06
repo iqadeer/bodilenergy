d8a78c41af04140eb5ce159eecba12561a87e15c
Imran Qadeer <imran.qadeer@it-route.co.uk>
2023-02-06 09:25:58 +0000

    changed default port to 5601

--- graphapi/src/config/config.ts
+++ graphapi/src/config/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const environment = process.env.NODE_ENV || 'development';
-const port = process.env.SERVER_PORT || 5600;
+const port = process.env.SERVER_PORT || 5601;
 const apiRoot = process.env.API_ROOT || 'graphql';
 
 const config = { port, environment, apiRoot };
